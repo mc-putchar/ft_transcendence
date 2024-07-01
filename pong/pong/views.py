c44f3989989fe5b27c6427beecfc127eb333ba7d
mcutura <126549052+mc-putchar@users.noreply.github.com>
2024-07-01 22:29:10 +0200

    add makefile and User model (#2)
    * hello cloud
    
    * 42OAuth done
    
    * add makefile
    
    * add basic user model
    
    ---------
    
    Co-authored-by: CarloCattano <freevibes@gmail.com>

--- pong/pong/views.py
+++ pong/pong/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 import secrets
 from django.shortcuts import render, redirect
 from django.template import loader
+from .models import User
 
 def index(request):
     state = secrets.token_urlsafe(32)
@@ -14,3 +15,19 @@ def redirect(request):
 
     template = loader.get_template('online_students.html')
     return HttpResponse(template.render())
+
+def users(request):
+    users = User.objects.all().values()
+    template = loader.get_template('users.html')
+    context = {
+        'users': users,
+    }
+    return HttpResponse(template.render(context, request))
+
+def profile(request, id):
+    user = User.objects.get(id=id)
+    template = loader.get_template('profile.html')
+    context = {
+        'user': users,
+    }
+    return HttpResponse(template.render(context, request))
