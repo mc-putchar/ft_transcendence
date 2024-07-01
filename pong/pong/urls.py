2e1bf8b3908ca4d5c77a72e0217fc7de7c01d1b8
CarloCattano <freevibes@gmail.com>
2024-07-01 19:14:20 +0000

    42OAuth done

--- pong/pong/urls.py
+++ pong/pong/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('pong/', include('pong.urls')),
+    path('redirect', views.redirect, name='redirect'),
+    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
