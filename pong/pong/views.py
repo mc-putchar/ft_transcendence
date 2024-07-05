99b7ed647921d2f7149a2e9dbcd306e72d4bdbdd
CarloCattano <freevibes@gmail.com>
2024-07-05 11:46:43 +0000

    Login stuff and styling

--- pong/pong/views.py
+++ pong/pong/views.py
@@ -1,5 +1,4 @@
 import secrets
-import requests
 from django.http import HttpResponse
 from django.conf import settings
 from django.shortcuts import render, redirect
@@ -11,22 +10,26 @@ from .auth import exchange_code_for_token, get_user_data
 logger = logging.getLogger('django')
 redirect_uri = settings.REDIRECT_URI
 
+
 def game(request):
     template = loader.get_template('game.html')
     return HttpResponse(template.render({}, request))
+
 
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
 
+
 def login(request):
     state = secrets.token_urlsafe(32)
-    request.session['oauth_state'] = state  # Save the state in session for CSRF protection
+    request.session['oauth_state'] = state
     client_id = settings.CLIENT_ID
     auth_url = f"https://api.intra.42.fr/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=public"
 
     auth_url_with_state = f"{auth_url}&state={state}"
     return redirect(auth_url_with_state)
+
 
 def loginExternal(request):
     username = request.user
@@ -42,30 +45,34 @@ def loginExternal(request):
             else:
                 return HttpResponse('Invalid login', status=401)
 
+    return render(request, 'registration/login.html', {'display_name': username, 'login': username})
 
-    return render(request, 'registration/login.html',{'display_name': username, 'login': username})
 
 def main(request):
-    return render(request,"main.html")
+    return render(request, "main.html")
+
 
 def enter(request):
-    # TODO - add a non 42 user login 
+    # TODO - add a non 42 user login
     user_data = request.session.get('user_data')
     if user_data:
         display_name = user_data['displayname']
-        login = user_data['login']
-        return render(request, 'main.html', {'display_name': display_name, 'login': user_data['login']})
+        return render(request, 'main.html', {'display_name': display_name,
+                                             'login': user_data['login']})
     else:
         return HttpResponse('Not authenticated', status=401)
 
-def redirect_view(request):         # Renamed to avoid conflict with `redirect` function from `django.shortcuts`
+
+# Renamed to avoid conflict with `redirect` function from `django.shortcuts`
+def redirect_view(request):
     template = 'student_info.html'
     code = request.GET.get('code')
     state = request.GET.get('state')
     saved_state = request.session.pop('oauth_state', None)
 
     if state != saved_state:
-        return HttpResponse('State mismatch. Possible CSRF attack.', status=400)
+        return HttpResponse('State mismatch. Possible CSRF attack.',
+                            status=400)
 
     access_token = exchange_code_for_token(code, redirect_uri)
 
@@ -80,6 +87,7 @@ def redirect_view(request):         # Renamed to avoid conflict with `redirect` 
     else:
         return HttpResponse('Failed to exchange code for access token', status=400)
 
+
 def users(request):
     users = User.objects.all().values()
     template = loader.get_template('users.html')
@@ -87,6 +95,7 @@ def users(request):
         'users': users,
     }
     return HttpResponse(template.render(context, request))
+
 
 def profile(request, id):
     user = AbstractUser.objects.get(id=id)
