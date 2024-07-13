e3425574757c8a31f3ab7fb22cc5c6e226ba26f3
CarloCattano <freevibes@gmail.com>
2024-07-13 23:14:20 +0200

    FUll way through merge 4v4

--- pong/static/js/chat.js
+++ pong/static/js/chat.js
@@ -1,94 +1,96 @@
 export function initWS() {
-    const roomName = "lobby";
-    const wsProtocol = window.location.protocol === "https:" ? "wss://" : "ws://";
+  const roomName = "lobby";
+  const wsProtocol = window.location.protocol === "https:" ? "wss://" : "ws://";
 
-    const chatSocket = new WebSocket(
-        wsProtocol + window.location.host + '/ws/chat/' + roomName + '/'
-    );
+  const chatSocket = new WebSocket(
+    wsProtocol + window.location.host + "/ws/chat/" + roomName + "/",
+  );
 
-    const username = document.querySelector('#chat-username').textContent;
+  const username = document.querySelector("#chat-username").textContent;
 
-    chatSocket.onopen = function(e) {
-        console.log('Chat socket connected');
+  chatSocket.onopen = function (e) {
+    console.log("Chat socket connected");
+  };
+
+  chatSocket.onmessage = function (e) {
+    const data = JSON.parse(e.data);
+    if (data.message) {
+      notifyUser(data.message);
+      document.querySelector("#chat-log").value +=
+        data.username + ": " + data.message + "\n";
     }
+    if (data.users_list) {
+      document.querySelector("#chat-userlist").innerHTML = "";
 
-    chatSocket.onmessage = function(e) {
-        const data = JSON.parse(e.data);
-        if (data.message) {
-            notifyUser(data.message);
-            document.querySelector('#chat-log').value += (data.username + ": " + data.message + '\n');
-        }
-        if (data.users_list) {
-            document.querySelector('#chat-userlist').innerHTML = '';
+      for (let user of data.users_list) {
+        const user_label = document.createElement("p");
+        user_label.textContent = user;
+        user_label.className = "user_label";
+        document.querySelector("#chat-userlist").appendChild(user_label);
+      }
+    }
+  };
 
-            for (let user of data.users_list) {
-                const user_label = document.createElement('p');
-                user_label.textContent = user;
-                user_label.className = 'user_label';
-                document.querySelector('#chat-userlist').appendChild(user_label);
-            }
-        }
-    };
+  chatSocket.onclose = function (e) {
+    if (!e.wasClean) console.error("Chat socket closed unexpectedly", e);
+  };
 
-    chatSocket.onclose = function(e) {
-        if (!e.wasClean) 
-            console.error('Chat socket closed unexpectedly', e);
-    };
+  document.querySelector("#chat-message-input").focus();
 
-    document.querySelector('#chat-message-input').focus();
+  document.querySelector("#chat-message-input").onkeyup = function (e) {
+    if (e.keyCode === 13) {
+      // enter, return
+      document.querySelector("#chat-message-submit").click();
+    }
+  };
 
-    document.querySelector('#chat-message-input').onkeyup = function(e) {
-        if (e.keyCode === 13) {  // enter, return
-            document.querySelector('#chat-message-submit').click();
-        }
-    };
+  document.querySelector("#chat-message-submit").onclick = function (e) {
+    const messageInputDom = document.querySelector("#chat-message-input");
+    const message = messageInputDom.value;
+    chatSocket.send(
+      JSON.stringify({
+        message: message,
+        username: username,
+      }),
+    );
+    messageInputDom.value = "";
+  };
 
-    document.querySelector('#chat-message-submit').onclick = function(e) {
-        const messageInputDom = document.querySelector('#chat-message-input');
-        const message = messageInputDom.value;
-        chatSocket.send(JSON.stringify({
-            'message': message,
-            'username': username
-        }));
-        messageInputDom.value = '';
-    };
+  window.chatSocket = chatSocket;
 
-    window.chatSocket = chatSocket;
-
-    // if user clicks on the userlist
-    document.querySelector('#chat-userlist').onclick = function(e) {
-        const user = e.target.textContent;
-        document.querySelector('#chat-message-input').value = '@' + user + ' ';
-        document.querySelector('#chat-message-input').focus();
-    };
+  // if user clicks on the userlist
+  document.querySelector("#chat-userlist").onclick = function (e) {
+    const user = e.target.textContent;
+    document.querySelector("#chat-message-input").value = "@" + user + " ";
+    document.querySelector("#chat-message-input").focus();
+  };
 }
-
 
 // check if a message is tagging our user
 function getMention(message) {
-    const username = document.querySelector('#chat-username').textContent;
-    const mention = '@' + username.trim();
-    let trim = message.trim(); 
-    let words = trim.split(' ');
-    const firstWord = words[0];
+  const username = document.querySelector("#chat-username").textContent;
+  const mention = "@" + username.trim();
+  let trim = message.trim();
+  let words = trim.split(" ");
+  const firstWord = words[0];
 
-    return firstWord === mention ? true : false;
+  return firstWord === mention ? true : false;
 }
 
 // Spawn a notification if the user is tagged
 function notifyUser(message) {
-    if (getMention(message)) {
-        createNotification(message);
-    }
+  if (getMention(message)) {
+    createNotification(message);
+  }
 }
 
-// A notification modal that pops up when the user is tagged 
+// A notification modal that pops up when the user is tagged
 function createNotification(message) {
-    const modal = document.createElement('div');
-    modal.className = 'modal';
-    modal.id = 'NotificationModal';
-    modal.style.display = 'block';
-    modal.innerHTML = `
+  const modal = document.createElement("div");
+  modal.className = "modal";
+  modal.id = "NotificationModal";
+  modal.style.display = "block";
+  modal.innerHTML = `
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
@@ -104,12 +106,12 @@ function createNotification(message) {
           </div>
     `;
 
-    document.body.appendChild(modal);
-    document.getElementById('NotificationModal').style.display = 'block';
+  document.body.appendChild(modal);
+  document.getElementById("NotificationModal").style.display = "block";
 
-    document.getElementById('closeNotif').onclick = function() {
-        document.getElementById('NotificationModal').style.display = 'none';
-        document.body.removeChild(modal);
-        return;
-    };
+  document.getElementById("closeNotif").onclick = function () {
+    document.getElementById("NotificationModal").style.display = "none";
+    document.body.removeChild(modal);
+    return;
+  };
 }
