4cfb7a9cf6147a412160f418188e9abbf6ddbb54
Timoti Fregni <timotif@gmail.com>
2024-07-16 18:28:30 +0200

    added fallback route to catch unrecognized urls

--- pong/core/urls.py
+++ pong/core/urls.py
@@ -3,12 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path("chat/", include("chat.urls")),
     path('', include('pong.urls')),
+    re_path(r'^(?!admin/).*$', lambda request: redirect("/")) # Catch all other URLs
 ]
 
 if settings.DEBUG:
