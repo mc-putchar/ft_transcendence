eb75a3fac482a80be8789d71bdc0e8886532a96f
CarloCattano <freevibes@gmail.com>
2024-07-22 19:17:13 +0200

    improved tester to go through until /duel accepted

--- pong/test_selenium.py
+++ pong/test_selenium.py
@@ -7,13 +7,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--window-size=360,900")
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19")
 
 driver = webdriver.Remote(
     command_executor='http://selenium:4444/wd/hub',
     options=chrome_options )
 
-
-def test_login_navigation(username="foo", password="bar"):
+def test_login_navigation(username="foo", password="foo"):
     try:
         driver.get("http://django:8000/")
         
@@ -21,7 +22,6 @@ def test_login_navigation(username="foo", password="bar"):
         login_link = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "a.btn-primary[data-link]"))
         )
-
         login_link.click()
         
         WebDriverWait(driver, 10).until(EC.url_contains("/login"))
@@ -41,25 +41,53 @@ def test_login_navigation(username="foo", password="bar"):
         ).click()
 
         time.sleep(1)
-        
+
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "a[href='/chat']"))
         ).click()
 
-        time.sleep(2)
 
-        # input id chat-message-input 
         message_field = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "chat-message-input"))
         ).send_keys("Hello World")
 
-        
-        # button chat-message-submit
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "chat-message-submit"))
         ).click()
 
-        time.sleep(2)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        driver.quit()
+        exit()
+
+def duel_user():
+    try:
+        time.sleep(0.5)
+        # get the chat input field
+        message_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "chat-message-input"))
+        ).send_keys("/duel foo")
+        
+        # send the message
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "chat-message-submit"))
+        ).click()
+
+        time.sleep(1)
+
+    except Exception as e:
+        print(e)
+        driver.quit()
+        exit()
+
+def accept_duel():
+    try:
+        time.sleep(0.5)
+        WebDriverWait(driver, 10).until(
+            # a modal with a <a> element btn-success class
+            EC.presence_of_element_located((By.CSS_SELECTOR, "a.btn-success"))
+        ).click()
 
     except Exception as e:
         print(e)
@@ -70,5 +98,10 @@ if __name__ == "__main__":
     test_login_navigation("foo", "foo")
     driver.switch_to.new_window('window')
     test_login_navigation("bar", "bar")
-    driver.quit()
+
+    duel_user()
+    driver.switch_to.window(driver.window_handles[0])
+    accept_duel()
+    # hangs here for manual testing
+
 
