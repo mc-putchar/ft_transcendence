05d4d2cfde2004c60fb05756b5746cc1b23b4e5d
Timoti Fregni <timotif@gmail.com>
2024-07-31 14:32:48 +0200

    see previous commit message: lost the test file and a bunch of things being clumsy with git. Now should be fine

---
+++ pong/blockchain/hardhat/test/PongTournament.js
@@ -0,0 +1,69 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("PongTournament", function() {
+	let owner, player1, player2, hardhatTournament;
+	const player1Name = "foo";
+	const player1Hash = 0;
+	const player2Name = "bar";
+	const player2Hash = 1;
+
+	before(async function() {
+		[owner, player1, player2] = await ethers.getSigners();
+		hardhatTournament = await ethers.deployContract("PongTournament");
+	});
+
+	describe("Deployment", function() {
+		it("Should record deployer's address", async function() {
+			expect(await hardhatTournament.owner()).to.equal(owner.address);
+		});
+	});
+
+	describe("Player Management", function() {
+		it("Should allow anyone to add players", async function() {
+			await hardhatTournament.connect(player1).addPlayer(player1Hash, player1Name);
+			await hardhatTournament.connect(player2).addPlayer(player2Hash, player2Name);
+			expect(await hardhatTournament.getPlayerName(player1Hash)).to.equal(player1Name);
+			expect(await hardhatTournament.getPlayerName(player2Hash)).to.equal(player2Name);
+		});
+
+		it("Should deny duplicate players", async function() {
+			await expect(hardhatTournament.addPlayer(player1Hash, player1Name)).to.be.revertedWith("Player already exists");
+		});
+	});
+
+	describe("Match Management", function() {
+		let players = [player1Hash, player2Hash];
+		let score = [10, 8];
+		it("Should allow owner to add a match", async function() {
+			await hardhatTournament.connect(owner).addMatch(0, 0, players, score, players[0]);
+			expect(await hardhatTournament.getMatchWinnerName(0)).to.equal(player1Name);
+		});
+		it("Should deny non-owner to add a match", async function() {
+			await expect(hardhatTournament.connect(player1).addMatch(1, 0, players, score, players[0])).to.be.revertedWith("Caller is not the owner");
+		});
+		it("Should deny duplicate matches", async function() {
+			await expect(hardhatTournament.addMatch(0, 0, players, score, players[0])).to.be.revertedWith("Match already exists");
+		});
+		it("Should not commit the match if the winner is invalid", async function() {
+			await expect(hardhatTournament.addMatch(1, 0, players, score, 2)).to.be.revertedWith("Winner is not correct");
+		});
+		it("Should not allow draws", async function() {
+			score = [10, 10];
+			await expect(hardhatTournament.addMatch(1, 0, players, score, players[0])).to.be.revertedWith("Draw games not supported");
+		});
+		it("Should not allow matches with more than 2 players", async function() {
+			players = [0, 1, 2];
+			await expect(hardhatTournament.addMatch(1, 0, players, score, players[0])).to.be.revertedWith("Registering only 1v1 games");
+		});
+	});
+	describe("Tournament Management", function() {
+		it("Should allow owner to create a tournament", async function() {
+			await hardhatTournament.connect(owner).createTournament(0, 0);
+			expect(await hardhatTournament.getTournamentWinnerName(0)).to.equal(player1Name);
+		});
+		it("Should deny non-owner to create a tournament", async function() {
+			await expect(hardhatTournament.connect(player1).createTournament(1, 0)).to.be.revertedWith("Caller is not the owner");
+		});
+	});
+});
