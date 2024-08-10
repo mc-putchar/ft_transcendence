205db6e23bb738800c32edf2d93a6628128136be
Martin Cutura <mcutura@student.42berlin.de>
2024-08-10 18:10:57 +0200

    start integrating blockchain

--- pong/blockchain/deploy.py
+++ pong/blockchain/deploy.py
@@ -1,7 +1,7 @@
-from blockchain_api import PongBlockchain
+from .blockchain_api import PongBlockchain
 import os
 
-def main():
+def deploy():
 	blockchain = PongBlockchain('http://blockchain:8545')
 	print("Connected") if blockchain.is_connected() else print("Connection failed")
 	account = blockchain.web3.eth.accounts[0]
@@ -10,6 +10,7 @@ def main():
 		raise ValueError("HARDHAT_PRIVATE_KEY environment variable not set")
 	blockchain.deploy_contract(account, hardhat_private_key, "PongTournament.sol")
 	print(f"Contract deployed at address: {blockchain.address}")
+	return blockchain
 
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# 	main()
