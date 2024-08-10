59bf423d8fa1e86dd8b46e1db42623ebfb254098
timotif <timotif@gmail.com>
2024-08-10 16:23:00 +0200

    custom amount of accounts created on the blockchain

--- pong/blockchain/hardhat/hardhat.config.js
+++ pong/blockchain/hardhat/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+    solidity: "0.8.24",
+    networks: {
+        hardhat: {
+            accounts: {
+                count: 33,
+                accountsBalance: '10000000000000000000',
+            }
+        }
+    }
 };
