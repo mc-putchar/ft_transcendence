5bbd603d214dd6e65a046880b53bcf5b27a8b281
Timoti Fregni <timotif@gmail.com>
2024-09-03 13:37:48 +0200

    autoassign blockchain address upon optin up until the max number of players

--- pong/blockchain/views.py
+++ pong/blockchain/views.py
@@ -3,6 +3,10 @@ import os
 from django.shortcuts import render
 from .blockchain_api import PongBlockchain, hash_player
 from pong.context_processors import get_user_from_token
+from api.models import Profile
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 # Create your views here.
 def index(request):
@@ -10,6 +14,7 @@ def index(request):
 
 def optin(request):
 	user = get_user_from_token(request)['user']
+	profile = Profile.objects.get(user=user)
 	if user is None:
 		return render(request, 'blockchain-optin.html', {'message': 'User not found'})
 	elif user.profile.blockchain_address is not None:
@@ -17,8 +22,18 @@ def optin(request):
 	else:
 		chain = PongBlockchain()
 		sender = chain.accounts[0]
-		status = chain.addPlayerSimple(sender, os.getenv('HARDHAT_PRIVATE_KEY'), hash_player([user.email, user.id]), user.username)
-		if status['status'] == 1:
-			return render(request, 'blockchain-optin.html', {'message': 'Opted in successfully'})
-		else:
+		try:
+			# the first n players get assigned an ETH address with some money
+			if user.profile.id <= len(chain.accounts):
+				status = chain.addPlayerFull(sender, os.getenv('HARDHAT_PRIVATE_KEY'), hash_player([user.email, user.id]), user.username, chain.accounts[user.profile.id])
+				profile.blockchain_address = chain.accounts[user.profile.id]
+				profile.save()
+			else:
+				status = chain.addPlayerSimple(sender, os.getenv('HARDHAT_PRIVATE_KEY'), hash_player([user.email, user.id]), user.username)
+				profile.blockchain_address = "0x0000000000000000000000000000000000000000"
+			if status['status'] == 1:
+				return render(request, 'blockchain-optin.html', {'message': 'Opted in successfully'})
+			else:
+				return render(request, 'blockchain-optin.html', {'message': 'Opt in failed'})
+		except ValueError as e:
 			return render(request, 'blockchain-optin.html', {'message': 'Opt in failed'})
