1207cc028ec12d988958f289b2d904f8b48b11ba
Carlo Cattano <ccattano@student.42berlin.de>
2024-09-30 22:22:20 +0200

    fix scroll

--- pong/static/js/router.js
+++ pong/static/js/router.js
@@ -118,6 +118,7 @@ class Router {
 	}
 
 	async route() {
+		document.body.style.overflow = 'auto';
 		this.oldHash = this.oldHash ?? window.location.hash;
 		this.game?.stopGame();
 		this.game4P?.stopGame();
